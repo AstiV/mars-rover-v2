0dbe7fb1ea072f76bf44d6b47c032133dbd95576
Astrid Varga <astrid.varga@gmx.de>
2018-07-25 10:08:39 +0200

    create rover object

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,12 @@
 // Rover Object Goes Here
+var rover = {
+  direction: ["N", "S", "E", "W"]
+}
+// TODO: The rover’s default direction will be "N" (north).
 
 // ======================
 
-// ======================
+// functions receive a rover object as an argument
 function turnLeft(rover){
   console.log("turnLeft was called!");
 }
